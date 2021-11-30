7f1f693f44d2ff3a61bb3529c58d96e5d6771d99
Sidney <sidney.croset@hotmail.com>
2021-11-30 14:18:44 +0100

    first push cv

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Cv from "./Cv";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cv />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
